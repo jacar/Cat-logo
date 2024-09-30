9e7fd63dab24d841cff4ef01461c80484ffb8102
Armando Jose Ovalle <creativemusicjacov@gmail.com>
2024-09-30 17:13:30 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,29 @@
+{
+  "modifiedTime": "2024-09-30T16:55:15.108Z",
+  "sheets": [
+    {
+      "sheetId": 1945290477,
+      "title": "Hoja1",
+      "index": 0,
+      "sheetType": "GRID",
+      "gridProperties": {
+        "rowCount": 1000,
+        "columnCount": 26,
+        "rowGroupControlAfter": true,
+        "columnGroupControlAfter": true
+      }
+    }
+  ],
+  "permissions": [
+    {
+      "role": "reader"
+    },
+    {
+      "emailAddress": "strongmeropower@gmail.com",
+      "role": "owner"
+    }
+  ],
+  "name": "DIAF ARMANDO",
+  "createdTime": "2024-05-06T13:21:23.065Z",
+  "id": "1AH0DwHZTVQ69w-w3xSJOxqbsGa5FF5DaeI9Oy1JiQWA"
+}
